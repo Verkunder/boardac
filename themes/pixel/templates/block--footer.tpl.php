8f031fef81a0a8478f8cef2c7715e81cff83f537
Verkunder <Verkunder@mail.ru>
2018-12-02 13:09:49 +0300

    add theme creative

--- themes/pixel/templates/block--footer.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-// $Id: block-footer.tpl.php,v 1.2 2009/09/16 18:30:36 blagoj Exp $
-?>
-<div id="block-<?php print $block->module .'-'. $block->delta; ?>" class="col1 block-<?php print $block->module ?>">
-  <?php if (isset($block->subject)): ?>
-    <h3><?php print $block->subject ?></h3>
-  <?php endif; ?>
-  <?php print $content ?>
-</div>
